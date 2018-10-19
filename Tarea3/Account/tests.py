1d4018f838cb1e7ebd3f09707676bfb7832ce37c
14-10526 <14-10526@usb.ve>
2018-10-19 17:31:20 -0400

    Se creo la clase seguridad y se aplico la primera prueba -Falla por funcion no definida

--- Tarea3/Account/tests.py
+++ Tarea3/Account/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+import unittest
+
 
 # Create your tests here.
+class TestSeguridad(unittest.TestCase):
+
+    def test_IngresoDeUsuario(self):         
+      
+        
+        '''
+        Paso 1 , Caso 1
+        Caso de Prueba : Una persona con email "luis14@hotmail.com" que intenta registrarse 
+        con la clave "212006Lop" .
+        Se invoca la funcion IngresarUsuario() 
+        
+		Resultado esperado : True 
+
+		Resultado Obtenido : Falla la invocacion -la funcion no ha sido definida
+
+        '''
+        self.assertEqual(Seguridad.IngresarUsuario("luis14@hotmail.com","212006Lop"), True)
+
+if __name__ == '__main__':
+	unittest.main()
