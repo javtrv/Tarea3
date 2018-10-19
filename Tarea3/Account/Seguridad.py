519ef2efe1950c6ce560b1dfc017db93534a784a
14-10526 <14-10526@usb.ve>
2018-10-19 18:02:03 -0400

    Se realiza la primera prueba de Registrar Usuario - Fallo porque la funcion no existe

---
+++ Tarea3/Account/Seguridad.py
@@ -0,0 +1,16 @@
+import re
+
+
+
+class Seguridad:
+    def __init__(self):
+        self.Diccionario = {} 
+ 
+
+    def RegistrarUsuario(self,email,password1,password2):
+        return 
+
+
+
+
+
--- Tarea3/Account/tests.py
+++ Tarea3/Account/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+import unittest
+from Seguridad import *
+
 
 # Create your tests here.
+class TestSeguridad(unittest.TestCase):
+    def setUp(self):
+        '''
+        Instancia el modul Seguridad 
+        de manera directa para controlar las pruebas
+        '''
+        self.prueba = Seguridad()
+
+    def test_RegistrarUsuario(self): 
+        '''
+        Paso 1 , Caso 1
+        Caso de Prueba : Una persona con email "luis14@hotmail.com" que intenta registrarse 
+        con la clave "212006Lop" .
+        Se invoca la funcion RegistrarUsuario() 
+        
+        Resultado esperado : True 
+
+        Resultado Obtenido : Falla la invocacion -la funcion no ha sido definida
+    
+
+        '''
+        self.assertEqual(self.prueba.RegistrarUsuario(email = "luis14@hotmail.com", password1 = "212006Lop",password2 ="212006Lop"), True)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
